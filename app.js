751f7730ce89d236c3ed0e6bdcbcf8ee1ae605f3
Odun Balogun <15163838+odbalogun@users.noreply.github.com>
2020-10-12 18:45:01 +0100

    Wrapped up fourth video

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue({
   el: '#vue-app', // el specifies the root element of the vue app
   data: {
     name: "Ohdeebee",
-    job: 'Software Engineer'
+    job: 'Software Engineer',
+    website: 'http://www.thenetninja.co.uk',
+    websiteTag: '<a href="http://www.thenetninja.co.uk">The Net Ninja</a>'
   },
   methods: {
     greet: function(timeOfDay){
