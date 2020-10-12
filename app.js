9a0dd3c9548e2bef1b6a4b87060c324b2a1ff836
Odun Balogun <15163838+odbalogun@users.noreply.github.com>
2020-10-12 18:20:46 +0100

    Wrapped up third video

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 new Vue({
   el: '#vue-app', // el specifies the root element of the vue app
   data: {
-    name: "Ohdeebee"
+    name: "Ohdeebee",
+    job: 'Software Engineer'
+  },
+  methods: {
+    greet: function(timeOfDay){
+      // you can access values from data by using this.data.job or just this.job
+      return 'Good ' + timeOfDay + ', ' + this.name
+    }
   }
 });
