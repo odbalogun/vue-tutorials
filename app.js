f1e4aac0b741beea478b06a283e2b787b09facbf
Odun Balogun <15163838+odbalogun@users.noreply.github.com>
2020-10-19 15:05:21 +0100

    Wrapped up fifth video

--- app.js
+++ app.js
@@ -4,12 +4,30 @@ new Vue({
     name: "Ohdeebee",
     job: 'Software Engineer',
     website: 'http://www.thenetninja.co.uk',
-    websiteTag: '<a href="http://www.thenetninja.co.uk">The Net Ninja</a>'
+    websiteTag: '<a href="http://www.thenetninja.co.uk">The Net Ninja</a>',
+    age: 30,
+    x: 0,
+    y: 0
   },
   methods: {
     greet: function(timeOfDay){
       // you can access values from data by using this.data.job or just this.job
       return 'Good ' + timeOfDay + ', ' + this.name
+    },
+
+    add: function(inc){
+      this.age += inc
+    },
+
+    subtract: function(dec){
+      this.age -= dec
+    },
+
+    updateXY: function(event){
+      // event is a default param made available whenever a function is triggered by an event occurence. It is automatically passed to the function
+      // run console.log(event) to see what is available in the event object
+      this.x = event.offsetX
+      this.y = event.offsetY
     }
   }
 });
